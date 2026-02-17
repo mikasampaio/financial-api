08f47c51eef54d8cfd9b259e7601e6a297ff827a
mika_sampaio <mikaeli.sampaio@sharkdev.com.br>
2026-02-17 11:20:57 -0300

    refactor: reorganizar importações no AppModule e remover AppService não utilizado

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from "@nestjs/common";
-import { AppService } from "./app.service";
-import { PrismaModule } from "./modules/prisma/prisma.module";
+import { PrismaModule } from "./prisma/prisma.module";
+import { CategoryModule } from "./modules/categories/category.module";
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, CategoryModule],
 })
 export class AppModule {}
